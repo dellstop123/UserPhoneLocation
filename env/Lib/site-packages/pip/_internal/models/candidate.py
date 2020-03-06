536853f1154e2478dac7dd5278cd68c2ee566261
Guneet Singh <guneetsinghbali@gmail.com>
2020-03-06 18:46:33 +0530

    add

--- env/Lib/site-packages/pip/_internal/models/candidate.py
+++ env/Lib/site-packages/pip/_internal/models/candidate.py
@@ -4,28 +4,33 @@ from pip._internal.utils.models import KeyBasedCompareMixin
 from pip._internal.utils.typing import MYPY_CHECK_RUNNING
 
 if MYPY_CHECK_RUNNING:
-    from pip._vendor.packaging.version import _BaseVersion  # noqa: F401
-    from pip._internal.models.link import Link  # noqa: F401
-    from typing import Any, Union  # noqa: F401
+    from pip._vendor.packaging.version import _BaseVersion
+    from pip._internal.models.link import Link
 
 
 class InstallationCandidate(KeyBasedCompareMixin):
     """Represents a potential "candidate" for installation.
     """
 
-    def __init__(self, project, version, location):
-        # type: (Any, str, Link) -> None
-        self.project = project
+    def __init__(self, name, version, link):
+        # type: (str, str, Link) -> None
+        self.name = name
         self.version = parse_version(version)  # type: _BaseVersion
-        self.location = location
+        self.link = link
 
         super(InstallationCandidate, self).__init__(
-            key=(self.project, self.version, self.location),
+            key=(self.name, self.version, self.link),
             defining_class=InstallationCandidate
         )
 
     def __repr__(self):
         # type: () -> str
         return "<InstallationCandidate({!r}, {!r}, {!r})>".format(
-            self.project, self.version, self.location,
+            self.name, self.version, self.link,
+        )
+
+    def __str__(self):
+        # type: () -> str
+        return '{!r} candidate (version {} at {})'.format(
+            self.name, self.version, self.link,
         )
