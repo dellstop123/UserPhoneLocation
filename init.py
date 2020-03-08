dd22efdfd86c1ca6d3c606b6d7d4051da073dbda
Guneet Singh <guneetsinghbali@gmail.com>
2020-03-08 18:17:42 +0530

    add

--- init.py
+++ init.py
@@ -1,10 +1,11 @@
 
 from flask import Flask, render_template,flash,request
 import requests
-import json
+import json,socket
 from random import random
 import reverse_geocoder as rg 
-from phonenumbers import carrier
+import phonenumbers
+from phonenumbers import carrier,timezone,geocoder
 import pprint
 
 URL = 'https://www.sms4india.com/api/v1/sendCampaign'
@@ -52,22 +53,34 @@ def number():
     # random_number = random.randint(1, 1000)
     print(data)
     """  Function To Print GeoIP Latitude & Longitude """
+    
     ip_request = requests.get('https://get.geojs.io/v1/ip.json')
     my_ip = ip_request.json()['ip']
     geo_request = requests.get('https://get.geojs.io/v1/ip/geo/' +my_ip + '.json')
     geo_data = geo_request.json()
     print({'latitude': geo_data['latitude'], 'longitude': geo_data['longitude']})
     coordinates =(geo_data['latitude'], geo_data['longitude']) 
-    ro_number = phonenumbers.parse(data, "RO")
-    print(carrier.name_for_number(ro_number, "en"))
+    ro = phonenumbers.parse(data,"RO")
+    host_name = socket.gethostname() 
+    host_ip = socket.gethostbyname(host_name) 
+    # rest = phonenumbers.PhoneMetadata.load_all()
+    # ro_number = phonenumbers.parse(data, "RO")
+    carr = carrier.name_for_number(ro, "en")
+    timez = timezone.time_zones_for_number(ro)
+    geo = geocoder.description_for_number(ro, "en")
+    print(carr)
+    print(timez)
+    print(geo)
+    print(request.remote_addr)
     sender = reverseGeocode(coordinates)
-    d = list(sender[0].values())[2]+','+list(sender[0].values())[3]+','+list(sender[0].values())[4]
+    map = './map.html'
+    d = "http://bit.ly/2Tz25pN"
     print(d)
         # get response
-    response = sendPostRequest(URL, 'VX456E299QDCERQ7BJV1YXO8IVS6N4ES', 'S3J2ROXIS9J5ZPKP','stage', data, 'SMSIND',d)
+    response = sendPostRequest(URL, 'EAAWTVR19VMQOWB6TGOU0KJX1LQ981VT', 'HIQ247XV8GRQ7Z31','stage', data, 'SMSIND',"Bas Kar kinni Gallan Krni aa")
     # print(response.text)
         # get response
-    response1 = sendGetRequest(URL, 'VX456E299QDCERQ7BJV1YXO8IVS6N4ES', 'S3J2ROXIS9J5ZPKP', 'stage', '7303820799', 'SMSIND', 'message-text' )
+    # response1 = sendGetRequest(URL, 'VX456E299QDCERQ7BJV1YXO8IVS6N4ES', 'S3J2ROXIS9J5ZPKP', 'stage', '7303820799', 'SMSIND', 'message-text' )
     """
     Note:-
         you must provide apikey, secretkey, usetype, mobile, senderid and message values
